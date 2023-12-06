e7f20f963abb79a3a4feb7fe48fb671205944094
Maxime <maximejacob4000@gmail.com>
2023-12-06 12:11:14 +0100

    Ajout de detail.php

--- Projet.php
+++ Projet.php
@@ -1,21 +1,24 @@
+
 <?php
-$xmlDoc = new DOMDocument();
+$catalogue = new DOMDocument();
 $testXML = file_get_contents("CD.xml");
-$xmlDoc->loadXML($testXML);
+$catalogue->loadXML($testXML);
 
-$cdList = $xmlDoc->getElementsByTagName("cd");
+$cdList = $catalogue->getElementsByTagName("cd");
 
-// Parcourir chaque CD et afficher les informations
 foreach ($cdList as $cd) {
     $titre = $cd->getElementsByTagName("titre")->item(0)->nodeValue;
     $artiste = $cd->getElementsByTagName("artiste")->item(0)->nodeValue;
     $pochette = $cd->getElementsByTagName("pochette")->item(0)->nodeValue;
 
-    // Affichage des informations
     echo "<div>";
     echo "<img src='$pochette' alt='$titre'>";
     echo "<p>Titre: $titre</p>";
     echo "<p>Artiste: $artiste</p>";
+    echo '<form action="detail.php?cd='.urlencode($titre).'" method="get">';
+    echo '<button type="submit">Choisir</button>';
+    echo '</form>';
     echo "</div>";
+
 }
 ?>

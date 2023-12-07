92b06fa17b93cf3c24d54a15f43cb9bb27bbbddb
Samuel <Samuel>
2023-12-07 12:29:25 +0100

    Ajout dune page pour paiement

--- genererImage.php
+++ genererImage.php
@@ -1,8 +1,10 @@
 <?php
     header("Content-type: image/jpeg");
     //$idCD = $_GET['idCD'];
-    $pochette = $cdList[0]->getElementsByTagName("pochette")->item(0)->nodeValue;
-    $im = imagecreatefromjpeg($pochette);
+    //$pochette = $cdList[0]->getElementsByTagName("pochette")->item(0)->nodeValue;
+    //$im = imagecreatefromjpeg("images/dark_side_of_the_moon.jpg");
+    $im = imagecreatefromjpeg($cdList[0]->getElementsByTagName("pochette")->item(0)->nodeValue);
+    
     //imagescale($im,200,200);
     imagejpeg($im);
     imagedestroy($im);

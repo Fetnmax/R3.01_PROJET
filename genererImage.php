7a38419ae132aa1a1919f9c7fd6160d61c77d749
Maxime <maximejacob4000@gmail.com>
2023-12-07 12:28:45 +0100

    generer image (presque)

--- genererImage.php
+++ genererImage.php
@@ -1,7 +1,12 @@
 <?php
     header("Content-type: image/jpeg");
-    //$idCD = $_GET['idCD'];
-    $pochette = $cdList[0]->getElementsByTagName("pochette")->item(0)->nodeValue;
+    $idCD = $_GET['idCD'];
+    $catalogue = new DOMDocument();
+    $testXML = file_get_contents("CD.xml");
+    $catalogue->loadXML($testXML);
+
+    $cdList = $catalogue->getElementsByTagName("cd");
+    $pochette = $cdList[$idCD]->getElementsByTagName("pochette")->item(0)->nodeValue;
     $im = imagecreatefromjpeg($pochette);
     //imagescale($im,200,200);
     imagejpeg($im);

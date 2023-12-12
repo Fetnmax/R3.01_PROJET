9044e3770e112464201e188ede3b0ab5ae439156
Maxime <maximejacob4000@gmail.com>
2023-12-12 18:25:49 +0100

    modif bdd

--- genererImage.php
+++ genererImage.php
@@ -21,33 +21,4 @@
     imagejpeg($im);
     imagedestroy($im);
 
-    /*$catalogue = new DOMDocument();
-$testXML = file_get_contents("CD.xml");
-$catalogue->loadXML($testXML);
-
-$cdList = $catalogue->getElementsByTagName("cd");
-
-$id = 0;
-
-echo "<h2>Catalogue</h2>";
-echo "<div class='mesAlbums'>";
-// Parcourir chaque CD et afficher les informations
-foreach ($cdList as $cd) {
-    $titre = $cd->getElementsByTagName("titre")->item(0)->nodeValue;
-    $artiste = $cd->getElementsByTagName("artiste")->item(0)->nodeValue;
-    $pochette = $cd->getElementsByTagName("pochette")->item(0)->nodeValue;
-
-    // Affichage des informations
-    echo "<div class='containerAlbum'>";
-    echo "<img src='genererImage.php?idCD=$id' alt='$titre'>";
-    echo "<p class='TitreAlbum'>Titre: $titre</p>";
-    echo "<p class='ArtisteAlbum'>Artiste: $artiste</p>";
-    echo '<form action="detail.php" method="get">';
-    echo '<button type="submit" name="idCD" value="'.$id.'">Choisir</button>';
-    echo '</form>';
-    echo "</div>";
-
-    $id++;
-}
-echo "</div>";*/
 ?>

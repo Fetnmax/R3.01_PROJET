31564693ffd2d02e1bec998f51f069a2a040dc27
Maxime <maximejacob4000@gmail.com>
2023-12-12 17:56:40 +0100

    modification de tout pour la bdd

--- Projet.php
+++ Projet.php
@@ -8,35 +8,35 @@
 </head>
 <body>
 <?php
-$catalogue = new DOMDocument();
-$testXML = file_get_contents("CD.xml");
-$catalogue->loadXML($testXML);
+    $bdd= "scurran_bd"; // Base de données
+    $host= "lakartxela.iutbayonne.univ-pau.fr";
+    $user= "scurran_bd"; // Utilisateur
+    $pass= "scurran_bd"; // mp
+    $nomtable= "CD"; /* Connection bdd */
+    $link=mysqli_connect($host,$user,$pass,$bdd) or die( "Impossible de se connecter à la base de données");
+    $query = "SELECT * FROM CD ORDER BY id";
+    $result= mysqli_query($link,$query);
 
-$cdList = $catalogue->getElementsByTagName("cd");
+    echo "<h2>Catalogue</h2>";
+    echo "<div class='mesAlbums'>";
+    // Parcourir chaque CD et afficher les informations
+    while ($donnees=mysqli_fetch_assoc($result)) {
+        $id = $donnees['id'];
+        $titre = $donnees['titre'];
+        $artiste = $donnees['artiste'];
+        $pochette = $donnees['pochette'];
 
-$id = 0;
-
-echo "<h2>Catalogue</h2>";
-echo "<div class='mesAlbums'>";
-// Parcourir chaque CD et afficher les informations
-foreach ($cdList as $cd) {
-    $titre = $cd->getElementsByTagName("titre")->item(0)->nodeValue;
-    $artiste = $cd->getElementsByTagName("artiste")->item(0)->nodeValue;
-    $pochette = $cd->getElementsByTagName("pochette")->item(0)->nodeValue;
-
-    // Affichage des informations
-    echo "<div class='containerAlbum'>";
-    echo "<img src='genererImage.php?idCD=$id' alt='$titre'>";
-    echo "<p class='TitreAlbum'>Titre: $titre</p>";
-    echo "<p class='ArtisteAlbum'>Artiste: $artiste</p>";
-    echo '<form action="detail.php" method="get">';
-    echo '<button type="submit" name="idCD" value="'.$id.'">Choisir</button>';
-    echo '</form>';
+        // Affichage des informations
+        echo "<div class='containerAlbum'>";
+        echo "<img src='genererImage.php?idCD=$id' alt='$titre'>";
+        echo "<p class='TitreAlbum'>Titre: $titre</p>";
+        echo "<p class='ArtisteAlbum'>Artiste: $artiste</p>";
+        echo '<form action="detail.php" method="get">';
+        echo '<button type="submit" name="idCD" value="'.$id.'">Choisir</button>';
+        echo '</form>';
+        echo "</div>";
+    }
     echo "</div>";
-
-    $id++;
-}
-echo "</div>";
 ?>
 </body>
 </html>
--- genererImage.php
+++ genererImage.php
@@ -1,14 +1,23 @@
 <?php
     header("Content-type: image/jpeg");
-    $idCD = $_GET['idCD'];
-    $catalogue = new DOMDocument();
-    $testXML = file_get_contents("CD.xml");
-    $catalogue->loadXML($testXML);
 
-    $cdList = $catalogue->getElementsByTagName("cd");
-    $pochette = $cdList[$idCD]->getElementsByTagName("pochette")->item(0)->nodeValue;
+    $idCD = $_GET['idCD'];
+    
+    $bdd= "scurran_bd"; // Base de données
+    $host= "lakartxela.iutbayonne.univ-pau.fr";
+    $user= "scurran_bd"; // Utilisateur
+    $pass= "scurran_bd"; // mp
+    $nomtable= "CD"; // Connection bdd 
+    $link=mysqli_connect($host,$user,$pass,$bdd) or die( "Impossible de se connecter à la base de données");
+    $query = "SELECT * FROM CD WHERE id = $idCD";
+    $result= mysqli_query($link,$query);
+    
+    while ($donnees=mysqli_fetch_assoc($result)) {
+        $pochette = $donnees['pochette'];
+    }
+
     $im = imagecreatefromjpeg($pochette);
-    //imagescale($im,200,200);
+    $im = imagescale($im,200,200);
     imagejpeg($im);
     imagedestroy($im);
 ?>
